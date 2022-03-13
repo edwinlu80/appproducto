22ee0b20e1e3c6acdde1a49d8839376871f1ea5e
edwinlu80 <elagunau@qbe.net.pe>
2022-03-12 19:25:19 -0500

    primer commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using appproducto.Models;
+
+namespace appproducto.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+    public IActionResult store(Producto producto)
+    {
+        ViewData["Message"] = "Datos se almacenaron correctamente";
+        return View("Index", producto);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Models/Producto.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace appproducto.Models
+{
+    public class Producto
+    {
+        public string UnidadMedida {get; set; }
+        public double Precio {get; set; }
+        public string Nombre {get; set; }
+    }
+}
